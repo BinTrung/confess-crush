fbeac977b2f5b1dd2a65f8840905782057e36b26
BinTrung <bintrung1110@gmail.com>
2024-12-28 02:06:33 +0700

    Xin Lỗi Chi

--- config.js
+++ config.js
@@ -1,20 +1,18 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
+    titleWeb: "Bin xin lỗi Chi",
+    introTitle: 'Hoàng Thị Kim Chi',
+    introDesc: `Bin có lỗi với c Chi
+    Cho e xin lỗi c nhaaaaa :((( 
+    xin lỗi nhìu nhìu ạ...`,
+    btnIntro: 'ò',
+    title: 'Phải chăng lúc ấy hỏng có ngẩu :<<<< ',
+    desc: 'Thì bây h c Chi đã hỏng có giận hiccc :<<<<< ',
+    btnYes: 'Thì giận đó sao',
     btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
+    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại giận em :<<<',
     btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
+    reply: 'thì yêu mới giận ghét mới thương nên là giận thì cũng tha lỗi',
+    mess: 'E biết mà 🥰. Yêu ChiChi nhiều nhiều 😘😘',
+    messDesc: 'C Chi siu dth tha lỗi cho e nhaaaaaa.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
-}
+  
